97843261bea0c0f855d25f4055f4c741f2a37b6b
capt_carl <captcarl13@gmail.com>
2018-09-13 02:18:40 -0400

    forgot

--- updates.sh
+++ updates.sh
@@ -10,7 +10,7 @@ sleep 1
 #If brew is not installed uncomment the next line or copy/paste it into a new term
 # /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 ECHO Updating Homebrew...
-brew update
+brew update &
 sleep 1
 ECHO The following formulae are OUTDATED and will be UPGRADED:
 brew outdated && brew upgrade
