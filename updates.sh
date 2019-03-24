277ee0554af2c73813fcc4ed5b0e25078b500b36
capt_carl <captcarl13@gmail.com>
2019-03-24 15:25:29 -0400

    Moved a ton of stuff around.

--- updates.sh
+++ updates.sh
@@ -3,30 +3,28 @@ sleep 1
 #Homebrew is a third-party package manager
 #If brew is not installed uncomment the next line or copy/paste it into a new term
 # /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
-ECHO Updating Homebrew... && brew update && sleep 1
-ECHO The following formulae are OUTDATED and will be UPGRADED: && sleep 1
+brew update & ECHO Updating Homebrew...
+sleep 1 &&
+ECHO The following formulae are OUTDATED and will be UPGRADED: & sleep 1
 brew outdated && brew upgrade
 sleep 1
 #PowerShell for macOS updater here
-ECHO Updating PowerShell for macOS...Enter Administrator password if prompted...
-sleep 1
-brew cask upgrade powershell
+brew cask upgrade powershell & ECHO Updating PowerShell for macOS...enter Administrator password if prompted...
 sleep 1
 #Atom IDE upgrade phase begins here
-ECHO Updating Atom packages... & apm upgrade --no-confirm
+apm upgrade --no-confirm & ECHO Updating Atom packages...
 sleep 1
 ECHO Checking for macOS system, security, and core App updates...
 sleep 1
 #sudo is required for softwareupdate
 tput bel & afplay /System/Library/Sounds/Glass.aiff & ECHO Please enter Administrator password:
 #softwareupdate is Apple's CLI update utility
-sudo softwareupdate --verbose --all -ia
-ECHO Checking and updating App Store Apps...
+sudo softwareupdate --verbose --all -ia & ECHO Checking and updating App Store Apps...
 #mas-cli is a Homebrew package which updates third-party App Store apps
 #if mas-cli is not installed, uncomment the next line
 #brew install mas
 #tenatively uncommenting mas seems to work on Mojave
-mas outdated && sleep 30
+mas outdated & sleep 30
 mas upgrade
 sleep 1
 #Software update phase ends here
@@ -38,7 +36,7 @@ tput bel & read -r -p "Would you like to reboot? [y/N] " response
     then
       ECHO Please enter Administrator password if prompted:
       wait 1
-      sudo shutdown -r +1 & ECHO Rebooting...
+      ECHO Rebooting... & sudo shutdown -r +1
       exit 0
     else
       ECHO Done!
