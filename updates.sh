82e2c8a2b3bf0a69776d2b6241df16ff4838c92a
capt_carl <captcarl13@gmail.com>
2019-03-24 15:44:06 -0400

    Update updates.sh
    Rolled back changes from today to continue on testing.

--- updates.sh
+++ updates.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
+ECHO Checking for macOS system, security, and core App updates...
+sleep 1
+#sudo is required for softwareupdate
+tput bel & afplay /System/Library/Sounds/Glass.aiff & ECHO Please enter Administrator password:
+#softwareupdate is Apple's CLI update utility
+sudo softwareupdate --verbose --all -ia
 sleep 1
 #Homebrew is a third-party package manager
 #If brew is not installed uncomment the next line or copy/paste it into a new term
@@ -10,6 +16,7 @@ sleep 1
 #PowerShell for macOS updater here
 brew cask upgrade powershell & ECHO Updating PowerShell for macOS...enter Administrator password if prompted...
 sleep 1
+<<<<<<< HEAD
 #Atom IDE upgrade phase begins here
 apm upgrade --no-confirm & ECHO Updating Atom packages...
 sleep 1
@@ -19,6 +26,9 @@ sleep 1
 tput bel & afplay /System/Library/Sounds/Glass.aiff & ECHO Please enter Administrator password:
 #softwareupdate is Apple's CLI update utility
 sudo softwareupdate --verbose --all -ia & ECHO Checking and updating App Store Apps...
+=======
+ECHO Checking and updating App Store Apps...
+>>>>>>> parent of 4b63c63... Moved softwareupdate down to the bottom.
 #mas-cli is a Homebrew package which updates third-party App Store apps
 #if mas-cli is not installed, uncomment the next line
 #brew install mas
@@ -27,6 +37,25 @@ mas outdated & sleep 30
 mas upgrade
 sleep 1
 #Software update phase ends here
+#Atom IDE upgrade phase begins here
+ECHO Updating Atom packages...
+apm upgrade --no-confirm
+#Question about Syncing Google Drive with iCloud Drive
+#This section can be commented out or removed if moving away from a Mac at work
+#tput bel & afplay /System/Library/Sounds/Glass.aiff & read -r -p "Are you at work? [y/N] " response
+#  if [[ "$response" =~ ^([yY][eE]|[yY])+$ ]]
+#    then
+#      sleep 1
+#    else
+#      ECHO Syncing Google Drive share...
+#      sleep 1
+#      #rsync begins here
+#      rsync -varh --progress --delete --exclude .DS_Store --exclude /Apple\ TV\ Photo\ Cache/ ~/Library/Mobile\ Documents/com~apple~CloudDocs/Metroid/ ~/Google\ Drive/Metroid
+#      #rsync ends here
+#      sleep 1
+#  fi
+#GDrive and iCloud Drive sync ends here
+#Comment out or remove previous section if moving away from a Mac at work
 afplay /System/Library/Sounds/Glass.aiff & ECHO Updates complete!
 sleep 1
 #Reboot prompt begins here
