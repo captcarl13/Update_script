1e5e329eece6dc4649181d7d7123c1767518e486
Carl Viñas <captcarl13gmail.com>
2022-11-21 20:07:10 +0000

    added blank echo to finally put this to bed.

--- updatepi.sh
+++ updatepi.sh
@@ -6,8 +6,8 @@ sudo apt full-upgrade -y
 sudo apt autoremove -y
 wget https://www.internic.net/domain/named.root -qO- | sudo tee /var/lib/unbound/root.hints
 sleep 1
-#Reboot prompt begins here
 echo
+#Reboot prompt begins here
 read -r -p "Would you like to reboot? [y/N] " response
   if [[ "$response" =~ ^([yY][eE]|[yY])+$ ]]
 	then
