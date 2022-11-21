a2919c32416f024ec6ab66bb872d90b44f9a3de8
Carl Viñas <captcarl13gmail.com>
2022-11-21 19:44:40 +0000

    Fixed reboot section to make more Linux-specific.

--- updatepi.sh
+++ updatepi.sh
@@ -6,14 +6,14 @@ sudo apt full-upgrade -y
 sudo apt autoremove -y
 wget https://www.internic.net/domain/named.root -qO- | sudo tee /var/lib/unbound/root.hints
 #Reboot prompt begins here
-tput bel & read -r -p "Would you like to reboot? [y/N] " response
+read -r -p "Would you like to reboot? [y/N] " response
   if [[ "$response" =~ ^([yY][eE]|[yY])+$ ]]
 	then
-	  ECHO Please enter Administrator password if prompted:
+	  echo Please enter Administrator password if prompted:
 	  #wait 1
 	  sudo shutdown -r +1 & ECHO Rebooting...
 	  exit 0
 	else
-	  ECHO Done!
+	  echo Done!
   fi
 #Reboot prompt ends here
