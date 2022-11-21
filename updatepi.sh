1e58e2254d8b53fb9851357ebd2595fc4519e6e4
Carl Viñas <captcarl13gmail.com>
2022-11-21 19:45:52 +0000

    added wait between root.hints and reboot.

--- updatepi.sh
+++ updatepi.sh
@@ -5,6 +5,7 @@ sudo apt upgrade -y
 sudo apt full-upgrade -y
 sudo apt autoremove -y
 wget https://www.internic.net/domain/named.root -qO- | sudo tee /var/lib/unbound/root.hints
+wait 1
 #Reboot prompt begins here
 read -r -p "Would you like to reboot? [y/N] " response
   if [[ "$response" =~ ^([yY][eE]|[yY])+$ ]]
