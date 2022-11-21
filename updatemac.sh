2d5f8621e021b7f30771825ba49f36673e187da8
Carl Viñas <captcarl13gmail.com>
2022-11-21 20:41:54 +0000

    changed syntax for many commands in all files.

--- updatemac.sh
+++ updatemac.sh
@@ -1,37 +1,37 @@
 #!/bin/bash
 sleep 1
-#Homebrew is a third-party package manager
-#If brew is not installed uncomment the next line or copy/paste it into a new term
+# Homebrew is a third-party package manager
+# If brew is not installed uncomment the next line or copy/paste it into a new term
 #/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
-#Upgrade ohmyzsh
+# Upgrade ohmyzsh
 env ZSH=$ZSH /bin/sh $ZSH/tools/upgrade.sh
 sleep 1
-ECHO Updating and upgrading Homebrew packages...Enter Administrator password if prompted...
+echo Updating and upgrading Homebrew packages...Enter Administrator password if prompted...
 brew upgrade && brew upgrade --cask && sleep 1
-ECHO Checking for macOS system, security, and core App updates...
+echo Checking for macOS system, security, and core App updates...
 sleep 1
-#sudo is required for softwareupdate
-tput bel & afplay /System/Library/Sounds/Glass.aiff & ECHO Please enter Administrator password:
-#softwareupdate is Apple's CLI update utility
+# sudo is required for softwareupdate
+tput bel & afplay /System/Library/Sounds/Glass.aiff & echo Please enter Administrator password:
+# softwareupdate is Apple's CLI update utility
 sudo softwareupdate --verbose -ia
-#commenting out 22-26 until mas is fixed...
-#ECHO Checking and updating App Store Apps...
+# commenting out 22-26 until mas is fixed...
+#echo Checking and updating App Store Apps...
 #mas outdated & sleep 5
 #mas upgrade
 #sleep 1
 #Software update phase ends here
 afplay /System/Library/Sounds/Glass.aiff & ECHO Updates complete!
 sleep 1
-#Reboot prompt begins here
+# Reboot prompt begins here
 tput bel & read -r -p "Would you like to reboot? [y/N] " response
   if [[ "$response" =~ ^([yY][eE]|[yY])+$ ]]
     then
-      ECHO Please enter Administrator password if prompted:
+      echo Please enter Administrator password if prompted:
       #wait 1
-      sudo shutdown -r +1 & ECHO Rebooting...
+      sudo shutdown -r +1 & echo Rebooting...
       exit 0
     else
-      ECHO Done!
+      echo Done!
   fi
-#Reboot prompt ends here
-#end of script
+# Reboot prompt ends here
+# end of script
--- updatepi.sh
+++ updatepi.sh
@@ -1,23 +1,23 @@
 #!/bin/bash
-#comment out line 3 if zsh isn't your default shell
+# comment out line 3 if zsh isn't your default shell
 env ZSH=$ZSH /bin/sh $ZSH/tools/upgrade.sh
 sudo apt update
 sudo apt upgrade -y
 sudo apt full-upgrade -y
 sudo apt autoremove -y
-#comment out line 9 if you aren't running Pi-Hole+Unbound
+# comment out line 9 if you aren't running Pi-Hole+Unbound
 wget https://www.internic.net/domain/named.root -qO- | sudo tee /var/lib/unbound/root.hints
 sleep 1
 echo
 echo
-#Reboot prompt begins here
+# Reboot prompt begins here
 read -r -p "Would you like to reboot? [y/N] " response
   if [[ "$response" =~ ^([yY][eE]|[yY])+$ ]]
 	then
 	  echo Please enter Administrator password if prompted:
-	  	  sudo shutdown -r +1 & ECHO Rebooting...
+	  	  sudo shutdown -r +1 & echo Rebooting...
 	  exit 0
 	else
 	  echo Done!
   fi
-#Reboot prompt ends here
+# Reboot prompt ends here
