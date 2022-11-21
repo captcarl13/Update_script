8333e8e9a04490fde3b34186cfe4435af8ffa112
Carl Viñas <captcarl13gmail.com>
2022-11-21 19:47:48 +0000

    sleep, not wait

--- updatepi.sh
+++ updatepi.sh
@@ -5,14 +5,13 @@ sudo apt upgrade -y
 sudo apt full-upgrade -y
 sudo apt autoremove -y
 wget https://www.internic.net/domain/named.root -qO- | sudo tee /var/lib/unbound/root.hints
-wait 1
+sleep 1
 #Reboot prompt begins here
 read -r -p "Would you like to reboot? [y/N] " response
   if [[ "$response" =~ ^([yY][eE]|[yY])+$ ]]
 	then
 	  echo Please enter Administrator password if prompted:
-	  #wait 1
-	  sudo shutdown -r +1 & ECHO Rebooting...
+	  	  sudo shutdown -r +1 & ECHO Rebooting...
 	  exit 0
 	else
 	  echo Done!
