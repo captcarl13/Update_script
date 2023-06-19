7722d6c44feeb603facd30e8c02fdfa0f7e8716e
Carl Viñas <captcarl13gmail.com>
2023-06-19 19:22:26 -0400

    Created new “startmeup.sh” for first-time setup

---
+++ startmeup.sh
@@ -0,0 +1,30 @@
+#quick install script, use this to install basic tools for fresh install. update regularly as you remember to add more tools to first-time setup
+#tools in this script:
+#github cli
+#zsh
+#ohmyzsh
+#tmux
+#htop
+#neofetch
+sudo apt update
+sudo apt install tmux htop neofetch 
+
+#github cli
+type -p curl >/dev/null || (sudo apt update && sudo apt install curl -y)
+curl -fsSL https://cli.github.com/packages/githubcli-archive-keyring.gpg | sudo dd of=/usr/share/keyrings/githubcli-archive-keyring.gpg \
+&& sudo chmod go+r /usr/share/keyrings/githubcli-archive-keyring.gpg \
+&& echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/githubcli-archive-keyring.gpg] https://cli.github.com/packages stable main" | sudo tee /etc/apt/sources.list.d/github-cli.list > /dev/null \
+&& sudo apt update \
+&& sudo apt install gh -y
+
+#zsh and ohmyzsh
+sudo apt install zsh
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+#zsh plugins
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+
+
+#powerlevel10k REQUIRES OHMYZSH
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
