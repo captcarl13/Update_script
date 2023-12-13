a30229d424565d2233d88f13f68f43984302b47f
Carl Viñas <captcarl13gmail.com>
2023-12-12 21:47:07 -0500

    Added section for updating `cloudflared` if using.

--- updatepi.sh
+++ updatepi.sh
@@ -1,23 +1,34 @@
 #!/bin/bash
-# Upgrade ohmyzsh -- comment out below line if zsh isn't your default shell and aren't using ohmyzsh
+#Upgrade ohmyzsh -- comment out below line if zsh isn't your default shell and aren't using ohmyzsh
 $ZSH/tools/upgrade.sh
 sudo apt update
 sudo apt upgrade -y
 sudo apt full-upgrade -y
 sudo apt autoremove -y
-sleep 1
 ~/padd.sh -u
-# uncomment line 9 if you aren't running Pi-Hole+Unbound
-# wget https://www.internic.net/domain/named.root -qO- | sudo tee /var/lib/unbound/root.hints
+#uncomment line 9 if you aren't running Pi-Hole+Unbound
+#wget https://www.internic.net/domain/named.root -qO- | sudo tee /var/lib/unbound/root.hints
 sleep 1
 echo
 echo
-# Reboot prompt begins here
+#comment out 15-25 if you are not using cloudflared
+#echo Updating cloudflared...
+#sleep 1
+#wget https://github.com/cloudflare/cloudflared/releases/latest/download/cloudflared-linux-arm64
+#sudo systemctl stop cloudflared
+#sudo mv -f ./cloudflared-linux-arm64 /usr/local/bin/cloudflared
+#sudo chmod +x /usr/local/bin/cloudflared
+#sudo systemctl start cloudflared
+#cloudflared -v
+#sleep 2
+#sudo systemctl status cloudflared
+sleep 2
+#Reboot prompt begins here
 read -r -p "Would you like to reboot? [y/N] " response
   if [[ "$response" =~ ^([yY][eE]|[yY])+$ ]]
 	then
 	  echo Please enter Administrator password if prompted:
-	  	  sudo shutdown -r +1 & echo Rebooting...
+			sudo shutdown -r +1 & echo Rebooting...
 	  exit 0
 	else
 	  echo Done!
